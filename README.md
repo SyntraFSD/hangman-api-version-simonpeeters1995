44e34d7302ef4dcfedbbf30ec7af37a6a54f813b
Korneel Eeckhout <korneel@pienter.tech>
2018-11-08 23:52:26 +0100

    init

---
+++ README.md
@@ -0,0 +1,51 @@
+# Oefening 04 - TEST
+## Voorbereiding:
+Clone de git-repository uit github en maak je eigen branch.
+1. Kopieer de url via github naar je klembord:  
+   ![Copy url](https://raw.githubusercontent.com/SyntraFSD-JS/ex-01/master/images/copy_github.png "Copy url to clipboard")
+
+Gebruik de terminal uit VSCode 
+(zorg dat je in de juiste folder, die waar je alle oefeningen opslaat, zit in VSCode)
+ om de volgende commands uit te voeren.
+(vervang `[naam]` door je eigen naam)
+
+1. `git clone https://github.com/SyntraFSD-JS/ex-03.git ex-04`
+2. `cd ex-04`
+3. `git checkout -b [naam]`
+4. `git push --set-upstream origin [naam]`
+
+Push regelmatig de changes die je maakt naar github
+
+1. Commit je changes:
+   1. Druk `Ctrl-Shift-G` om de Source Control tab te openen
+   2. Geef een beschrijving van de changes die je deed en duw `Ctrl-Enter`
+2. Push je changes:
+   1. Druk `Ctrl-Shift-P` om het Command Palette te openen
+   2. Zoek naar `Git: Push` en druk op `Enter` om uit te voeren 
+   
+Pull regelmatig om opmerkingen of aanpassingen van mij binnen te halen
+1. Druk `Ctrl-Shift-P` om het Command Palette te openen
+2. Zoek naar `Git: Pull` en druk op `Enter` om uit te voeren 
+     
+## Structuur
+De oefeningen bevinden zich in een aparte map met de naam `ex[oef. nummer]_[oef. naam]`
+Daarin zitten alle bestanden die je nodig hebt om de oefening te maken, meestal is dit een `index.html` en een `script.js` bestand.
+Het script bestand zal al gelinkt zijn aan het html bestand. Zie:
+```html
+<script src="script.js"></script>
+```
+Normaal moet je niet in het html bestand meer werken maar enkel in `script.js`.
+Ook daar zal meestal al een basisstructuur aanwezig zijn waarbij in comments staat waar je zelf dingen moet aanvullen.
+
+Om het resultaat van je oefening te bekijken open je het `index.html` bestand in chrome.
+In deze module gebruiken we allemaal chrome zodat we allemaal hetzelfde resultaat hebben.
+
+## Oefeningen:
+### Oefening 04: Hangman
+#### Doel:
+Hangman, nothing more, nothing less..
+
+
+
+   
+      
---
+++ ex04_hangman/script.js
@@ -0,0 +1,158 @@
+const randomWords = [
+  'condition',
+  'bottom',
+  'lineage',
+  'trip',
+  'reporter',
+  'paper',
+  'colorful',
+  'agent',
+  'justify',
+  'torture',
+  'cap',
+  'earthflax',
+  'payment',
+  'research',
+  'picture',
+  'garage',
+  'honor',
+  'memorial',
+  'planet',
+  'biography',
+  'profound',
+  'rumor',
+  'gear',
+  'bedroom',
+  'orthodox',
+  'penalty',
+  'grief',
+  'promote',
+  'roof',
+  'suite',
+  'moving',
+  'killer',
+  'museum',
+  'essay',
+  'fever',
+  'dignity',
+  'shadow',
+  'enjoy',
+  'kill',
+  'shy',
+  'counter',
+  'pawn',
+  'button',
+  'bullet',
+  'skin',
+  'rate',
+  'shop',
+  'consider',
+  'other',
+  'prospect',
+];
+const hangManImage;
+const solutionContainer;
+const winOrLoseContainer;
+const letterContainer;
+const gameState = {
+  word: [],
+  hangman: 1,
+  turn: 1,
+  lettersFound: 0,
+  won: false,
+  lost: false,
+};
+
+function createNewSolutionLetter() {
+  const newDiv = document.createElement('div');
+  newDiv.classList.add('solution-letter');
+  return newDiv;
+}
+
+function selectRandomWord() {
+  // return random word from the randomWords array and split it up into an array
+}
+
+function emptySolutionContainer() {
+  // empty the solutionContainer (remove all .letter elements)
+}
+
+function fillSolutionContainer() {
+  // after emptying the solutionContainer
+  // fill it up with one solutionLetter (use createNewSolutionLetter)
+  // per letter in the current gamestate.word
+}
+
+function removeClassesFromAllLetters() {
+  // remove the failed and success classes from all .letter
+  // use [node-element].classList.remove();
+}
+
+function updateHangmanPicture() {
+  // change the hangman picture source to the appropriate image (gameState.hangman)
+  // the source of each image looks like this: 'images/hangman01.png'
+  // of course the number changes, from 01 to 09
+}
+
+
+function initGameState() {
+  // this function initialises the gameState and playfield (html)
+  // you do not have to change this function
+  gameState.word = selectRandomWord();
+  gameState.hangman = 1;
+  gameState.turn = 1;
+  gameState.lettersFound = 0;
+  gameState.won = false;
+  gameState.lost = false;
+  fillSolutionContainer();
+  winOrLoseContainer.textContent = '';
+  removeClassesFromAllLetters();
+  updateHangmanPicture();
+}
+
+function winOrLose() {
+  // checks if the player has won or lost,
+  // if so the winOrLoseContainer text should be updated with an appropriate message
+}
+
+function letterClicked(event) {
+  // this is of course the heart of this game,
+  // find out which letter is clicked
+  // check if that letter is in in the current word (beware of upper/lowercase)
+  // if so fill in the .solution-letter (top tip: use '.solution-letter:nth-child(2)' in your querySelector)
+  // update the gameState
+  // add the correct class to the clicked letter (event.target)
+  // add 'success' when the letter is found
+  // add 'failed' when the letter is not (use [node-element].classList.add())
+  // don't forget to update the hangman picture
+  // make sure .letter with a success or .failed class can not be clicked
+
+  if (event.target.matches('.letter') && !gameState.lost && !gameState.won) {
+    if (!event.target.matches('.success') && !event.target.matches('.failed')) {
+      const selectedLetter = event.target.textContent;
+      let lettersFound = 0;
+      gameState.word.forEach(function (letter, index) {
+        if (letter.toUpperCase() === selectedLetter) {
+          lettersFound++;
+          const solutionLetter = document.querySelector('.solution-letter:nth-child(' + (index + 1) + ')');
+          solutionLetter.textContent = letter.toUpperCase();
+        }
+      });
+      gameState.turn++;
+      if (lettersFound > 0) {
+        gameState.lettersFound += lettersFound;
+        event.target.classList.add('success');
+      } else {
+        event.target.classList.add('failed');
+        gameState.hangman++;
+        updateHangmanPicture();
+      }
+      winOrLose();
+    }
+  }
+}
+
+initGameState();
+
+letterContainer.addEventListener('click', letterClicked);
+winOrLoseContainer.addEventListener('click', initGameState);
