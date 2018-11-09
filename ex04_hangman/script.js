fb99541bc0a3c2fcb442417b126fc24d22adb9c0
Matthias vdb <you@example.com>
2018-11-09 14:54:27 +0100

    3

--- ex04_hangman/script.js
+++ ex04_hangman/script.js
@@ -50,10 +50,10 @@ const randomWords = [
   'other',
   'prospect',
 ];
-const hangManImage;
-const solutionContainer;
-const winOrLoseContainer;
-const letterContainer;
+const hangManImage = document.querySelector('#image');
+const solutionContainer = document.querySelector('#solution-container');
+const winOrLoseContainer = document.querySelector('#win-lose-container');
+const letterContainer = document.querySelector('#letter-container');
 const gameState = {
   word: [],
   hangman: 1,
